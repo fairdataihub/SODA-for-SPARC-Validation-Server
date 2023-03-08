ac8d97deff24a4b00536fc0102a0b7ab98633aff
aaronm-2112 <aaron.marroquin96@gmail.com>
2023-03-08 13:54:32 -0800

    feat: add server shutdown

--- app.py
+++ app.py
@@ -20,6 +20,18 @@ configureRouteHandlers(api)
 
 api.init_app(app)
 
+@api.route("/sodaforsparc-validator_shutdown", endpoint="shutdown")
+class Shutdown(Resource):
+    def get(self):
+        func = request.environ.get("werkzeug.server.shutdown")
+        api.logger.info("Shutting down server")
+
+        if func is None:
+            print("Not running with the Werkzeug Server")
+            return
+
+        func()
+
 if __name__ == '__main__':
     api.logger.info(f"Starting server on port {9009}")
     app.run(host="127.0.0.1", port=9009)
